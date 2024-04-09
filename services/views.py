d5df3cdd8db12ed780ecca5a690845e1bb48afe1
justimmortal <hae.sooo@o2.pl>
2024-04-09 23:27:48 +0200

    add viev list

--- services/views.py
+++ services/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html', {'name': "Justina"})
+
+# lulu/views.py
+
+from .models import Service
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'service_list.html', {'services': services})
