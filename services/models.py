81ea41546d21c473b28ca0d07d8e59944e22834b
justimmortal <hae.sooo@o2.pl>
2024-04-10 01:25:56 +0200

    obrazki

--- services/models.py
+++ services/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
-    photoUrl = models.TextField(null = True)
+    image_url = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
