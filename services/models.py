aebd766cbd2906d63ebcadebae2af0a334f44225
dandeliusz27 <dandeliusz27@gmail.com>
2024-04-10 00:09:55 +0200

    photos var models added

--- services/models.py
+++ services/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
+    photoUrl = models.TextField(null = True)
 
     def __str__(self):
         return self.name
