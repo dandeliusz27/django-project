25f8cc28be88bdef9dac6eba53ef7188f6bac3c3
dandeliusz27 <dandeliusz27@gmail.com>
2024-04-10 20:59:09 +0200

    services list

--- services/urls.py
+++ services/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.category_list, name='category_list'),
+    path('service/', views.service_list, name='service_list'),
 ]
--- services/views.py
+++ services/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-from .models import Category
+from .models import Category, Service
 
 def category_list(request):
     categories = Category.objects.all()
     return render(request, 'category_list.html', {'categories': categories})
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'service_list.html', {'services': services})
