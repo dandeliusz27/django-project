1002991feb8b2cc9f3838d106f281b051c7eb36b
dandeliusz27 <dandeliusz27@gmail.com>
2024-04-10 23:20:27 +0200

    adding services

--- services/urls.py
+++ services/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.category_list, name='category_list'),
-    path('service/', views.service_list, name='service_list'),
+    path('service/<cat>', views.service_list, name='service_list'),
+    path('add_service/', views.add_service, name='add_service'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 
-from .models import Category, Service
+from .models import Category, Type, Service
 
 def category_list(request):
     categories = Category.objects.all()
     return render(request, 'category_list.html', {'categories': categories})
 
-def service_list(request):
-    services = Service.objects.all()
+def service_list(request, cat):
+    services = Service.objects.filter(category=cat)
     return render(request, 'service_list.html', {'services': services})
+
+def add_service(request):
+    if request.method == "POST":
+        category_id = request.POST['category']  # Poprawione
+        type_id = request.POST['types']  # Poprawione
+        user_id = request.user.id
+        
+        # Pobierz obiekty kategorii i typu na podstawie ich identyfikatorów
+        category = Category.objects.get(id=category_id)
+        type = Type.objects.get(id=type_id)
+        
+        # Utwórz nowy obiekt Service
+        service = Service(category=category, types=type, user_id=user_id)  # Poprawione
+        service.save()
+        return redirect('category_list')  # Poprawiona przekierowanie do odpowiedniego widoku
+    else:    
+        categories = Category.objects.all()
+        types = Type.objects.all()
+        return render(request, 'add_service.html', {'categories': categories, 'types': types})
